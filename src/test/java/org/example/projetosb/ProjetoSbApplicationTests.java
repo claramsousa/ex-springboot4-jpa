fdde638f2c11d43e052d96566530422b06ffd092
clara <clara3.macedo@gmail.com>
2026-02-05 10:46:53 -0300

    criação do projeto

---
+++ src/test/java/org/example/projetosb/ProjetoSbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projetosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
